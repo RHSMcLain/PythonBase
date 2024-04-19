cd0af03417d3980f4fe5961c222d05be97804034
amclain@riverdale.k12.or.us <amclain@riverdale.k12.or.us>
2024-04-19 12:11:42 -0700

    Working on integrating UDP listen loop with tkinter mainloop

--- baseConn.py
+++ baseConn.py
@@ -1,5 +1,8 @@
 import socket
+from tkinter import *
+from tkinter import ttk
 from Drone import Drone
+
 
 sendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -8,6 +11,18 @@ drones = []
 # UDP_IP = "192.168.50.124"
 UDP_IP = "10.127.174.114"
 UDP_PORT = 5005
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="hello world").grid(column = 0, row = 0)
+ttk.Label(frm, text="Drones List").grid(column = 0, row = 1)
+droneList = Listbox(frm)
+
+droneList.grid(column = 0, row = 2)
+
+
+
 
 def handshake(parts, addr):
     i = int(parts[1])
@@ -34,23 +49,31 @@ def listDrones():
     for drone in drones:
         print(drone.name, drone.ipAddress, drone.port, "\t") 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(0)
 sock.bind((UDP_IP, UDP_PORT))
-print("Ready3")
-while True:
-    
-    data, addr = sock.recvfrom(1024)
-    strData = data.decode("utf-8")
-    print("Received message %s" % data)
-    print(addr)
-    
-    print("----")
-    print(strData)
-    
-    parts = strData.split("|")
-    print(parts)
-    cmd = parts[0]
 
-    if cmd == "HND":
-        #HANDSHAKE
-        handshake(parts, addr)
+print("Ready3")
+def listen():
+    try:
+        data, addr = sock.recvfrom(1024)
+        strData = data.decode("utf-8")
+        print("Received message %s" % data)
+        print(addr)
+        
+        print("----")
+        print(strData)
+        
+        parts = strData.split("|")
+        print(parts)
+        cmd = parts[0]
+
+        if cmd == "HND":
+            #HANDSHAKE
+            handshake(parts, addr)
+    except:
+        pass
+    root.after(0, listen)
     
+root.after(0, listen)
+
+root.mainloop()
